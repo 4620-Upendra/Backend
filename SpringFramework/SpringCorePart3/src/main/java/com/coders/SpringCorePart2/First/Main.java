0b77472d16fb5effaff893139f5e3ab9e79df09d
4620-Upendra <Pediredlaupendrakumar@gmail.com>
2026-02-03 01:01:51 +0530

    Autowiring and its type

--- SpringFramework/SpringCorePart3/src/main/java/com/coders/SpringCorePart2/First/Main.java
+++ SpringFramework/SpringCorePart3/src/main/java/com/coders/SpringCorePart2/First/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context=new ClassPathXmlApplicationContext("com/coders/SpringCorePart2/Xml/exconfig.xml");
 		Student st1=(Student)context.getBean("std");
+		System.out.println("Bean created by using "+st1.getName());
 		System.out.println(st1);
 	}
 
